c076f2cb98239645171e436998044979fdc8adca
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2021-04-02 17:09:15 +0200

    Add recognition of already connected points

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ $(function(){
     const board = $('.board');
     const boardWidth = board.width();
     const boardHeight = board.height();
-    const step = 50;
+    let step = 50;
+    let shift = 3;
     let coordinateX = 0;
     let coordinateY = 0;
     let numberRows = boardWidth/step+1;
@@ -10,6 +11,9 @@ $(function(){
     let centerPointId = Math.floor(numberPoints/2);
     let centerPoint;
     let activePointsId = [-numberRows-1,-numberRows,-numberRows+1,-1,1,numberRows-1,numberRows,numberRows+1];
+    let previousConnectedId;
+    let newConnectedId;
+    let connections = [];
 
     // Add canvas
 
@@ -18,14 +22,16 @@ $(function(){
     canvas.width = boardWidth;
     canvas.height = boardHeight;
 
+    // Function to create points
+
     function creatPoints() {
         for(let i=0;i<numberPoints;i++){
             let point = $('<div></div>',{
                 'class':'point',
                 'id':i,
                 css:{
-                    'top':coordinateY-3,
-                    'left':coordinateX-3
+                    'top':coordinateY-shift,
+                    'left':coordinateX-shift
                 }
             })
             board.append(point);
@@ -39,34 +45,53 @@ $(function(){
         }
     }
 
+    // Function to return basic styles
+
+    function resetProperty() {
+        centerPoint.removeClass('center');
+        $('.active').removeClass('active').off('click', makeMove);
+    }
+
+    // Function to connect two points
+
     function makeMove() {
         
         let centerX = parseInt(centerPoint.css('left'));
         let centerY = parseInt(centerPoint.css('top'));
-        centerPoint.removeClass('center');
-        $('.active').removeClass('active').off('click', makeMove);
+        resetProperty();
         let newCenterPointId = Number($(this).attr('id'));
         let moveX = parseInt($('#'+newCenterPointId).css('left'));
         let moveY = parseInt($('#'+newCenterPointId).css('top'));
+
+        previousConnectedId = centerPoint.attr('id');
+        newConnectedId = $(this).attr('id');
+        connections.push(previousConnectedId+"-"+newConnectedId);
+
         drawMove(centerX,centerY,moveX,moveY);
         setActivePoints(newCenterPointId)
     }
+
+    // Function to set active points
 
     function setActivePoints(id) {
 
        centerPoint = $('#'+id);
        centerPoint.addClass('center');
        for(let i=0;i<activePointsId.length;i++){
-           let activePoint = $('#'+(id+activePointsId[i]));
-           activePoint.addClass('active');
-           activePoint.on('click', makeMove)
+            if(connections.includes(id+"-"+(id+activePointsId[i])) === false && connections.includes((id+activePointsId[i])+"-"+id) === false){ 
+                let activePoint = $('#'+(id+activePointsId[i]));
+                activePoint.addClass('active');
+                activePoint.on('click', makeMove)
+            }
        }
     }
 
+    // function to draw straight line
+
     function drawMove(centerX,centerY,moveX,moveY) {
         ctx.beginPath();
-        ctx.moveTo(centerX,centerY);
-        ctx.lineTo(moveX,moveY);
+        ctx.moveTo(centerX+shift,centerY+shift);
+        ctx.lineTo(moveX+shift,moveY+shift);
         ctx.lineWidth = 6;
         ctx.strokeStyle = "darkgreen";
         ctx.stroke();
