4329e998866a46e8f804c01b440f25c89b9233f7
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2021-04-02 13:42:37 +0200

    Add function to drawing motion

--- js/script.js
+++ js/script.js
@@ -9,7 +9,14 @@ $(function(){
     const numberPoints = numberRows*(boardHeight/step+1);
     let centerPointId = Math.floor(numberPoints/2);
     let centerPoint;
-    let activePointsId = [-numberRows-1,-numberRows,-numberRows+1,-1,1,numberRows-1,numberRows,numberRows+1]
+    let activePointsId = [-numberRows-1,-numberRows,-numberRows+1,-1,1,numberRows-1,numberRows,numberRows+1];
+
+    // Add canvas
+
+    const canvas = document.querySelector('canvas')
+    const ctx = canvas.getContext('2d');
+    canvas.width = boardWidth;
+    canvas.height = boardHeight;
 
     function creatPoints() {
         for(let i=0;i<numberPoints;i++){
@@ -29,19 +36,24 @@ $(function(){
             else if(coordinateX<boardWidth){
                 coordinateX+=step;
             }
-            
         }
     }
 
     function makeMove() {
         
+        let centerX = parseInt(centerPoint.css('left'));
+        let centerY = parseInt(centerPoint.css('top'));
         centerPoint.removeClass('center');
         $('.active').removeClass('active').off('click', makeMove);
         let newCenterPointId = Number($(this).attr('id'));
+        let moveX = parseInt($('#'+newCenterPointId).css('left'));
+        let moveY = parseInt($('#'+newCenterPointId).css('top'));
+        drawMove(centerX,centerY,moveX,moveY);
         setActivePoints(newCenterPointId)
     }
 
     function setActivePoints(id) {
+
        centerPoint = $('#'+id);
        centerPoint.addClass('center');
        for(let i=0;i<activePointsId.length;i++){
@@ -50,6 +62,16 @@ $(function(){
            activePoint.on('click', makeMove)
        }
     }
+
+    function drawMove(centerX,centerY,moveX,moveY) {
+        ctx.beginPath();
+        ctx.moveTo(centerX,centerY);
+        ctx.lineTo(moveX,moveY);
+        ctx.lineWidth = 6;
+        ctx.strokeStyle = "darkgreen";
+        ctx.stroke();
+    }
+
     creatPoints();
     setActivePoints(centerPointId);
 })
